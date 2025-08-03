2066caad50d71178a28800a43b54d2eac62fede7
mb bischoff <mb@mbbischoff.com>
2025-08-03 14:48:57 -0400

    Header

---
+++ src/data/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  title: "chromatographics",
+  subtitle: "gay poems by mb bischoff",
+  author: {
+    name: "mb bischoff",
+    url: "https://mbbischoff.com"
+  }
+} as const; 
